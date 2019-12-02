acc31c680fcd4dc0b6cf3888ff87cb621886dc33
github username <github email address>
2019-12-02 13:53:59 -0600

    BIG PUSH

--- App.js
+++ App.js
@@ -3,35 +3,73 @@ import { StyleSheet, Text, View } from 'react-native';
 
 import {createBottomTabNavigator} from 'react-navigation'
 
+import Icon from 'react-native-vector-icons/Ionicons'
 import Explore from './screens/Explore'
 import Saved from './screens/Saved'
 import Inbox from './screens/Inbox'
 import Trips from './screens/Trips'
-class App extends React.Component {
-  render(){
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-  }
-}
+import Profile from './screens/Profile'
+
 
 export default createBottomTabNavigator({
   Explore:{
-    screen:Explore
+    screen:Explore,
+    navigationOptions:{
+      tabBarLabel:'EXPLORE',
+      tabBarIcon:({tintColor})=>(
+        <Icon name="ios-search" color={tintColor} size={24} />
+      )
+    }
   },
   Saved:{
-    screen:Saved
+    screen:Saved,
+    navigationOptions:{
+      tabBarLabel:'SAVED',
+      tabBarIcon:({tintColor})=>(
+        <Icon name="ios-heart" color={tintColor} size={24} />
+      )
+    }
   },
   Trips:{
-    screen:Trips
+    screen:Trips,
+    navigationOptions:{
+      tabBarLabel:'TRIPS',
+      tabBarIcon:({tintColor})=>(
+        <Icon name="ios-map" color={tintColor} size={24} />
+      )
+    }
   },
   Inbox:{
-    screen:Inbox
+    screen:Inbox,
+    navigationOptions:{
+      tabBarLabel:'INBOX',
+      tabBarIcon:({tintColor})=>(
+        <Icon name="ios-mail" color={tintColor} size={24} />
+      )
+    }
+  },
+  Profile:{
+    screen:Profile,
+    navigationOptions:{
+      tabBarLabel:'PROFILE',
+      tabBarIcon:({tintColor})=>(
+        <Icon name="ios-person" color={tintColor} size={24} />
+      )
+    }
+  },
+},{
+  tabBarOptions:{
+    activeTintColor:'red',
+    inactivveTintColor:'grey',
+    style:{
+      backgroundColor:'white',
+      borderTopWidth:0,
+      shadowOffset:{width:5,height:3},
+      shadowColor:'black',
+      shadowOpacity: 0.5,
+      elevation: 5
+    }
   }
-
-
 })
 
 const styles = StyleSheet.create({
